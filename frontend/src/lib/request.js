f68fd35cd0f4596d4e38601a9ce96a4ac7742b04
Ratul Hossain <ratulhosainbnc@gmamil.com>
2025-05-22 02:04:21 +0600

    fix 3

--- frontend/src/lib/request.js
+++ frontend/src/lib/request.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const request = async ({ method = "get", url, data, params }) => {
   const response = await axios.request({
     method,
-    url: `https://plant-management-backend-fswh.onrender.com/${url}`,
+    url: `https://plant-management-backend-fswh.onrender.com${url}`,
     data,
     params,
   });
