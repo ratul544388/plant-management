046b6e6beffbd657a4e81c12fc5cecc08d482942
Ratul Hossain <ratulhosainbnc@gmamil.com>
2025-05-22 01:03:15 +0600

    fix

--- backend/src/routes/plant.routes.js
+++ backend/src/routes/plant.routes.js
@@ -1,25 +1,29 @@
-import express from 'express';
+import express from "express";
 import {
   createPlant,
   deletePlant,
   getAllPlants,
   getPlantById,
   getPlantsByUserEmail,
-  updatePlant
-} from '../controllers/plant.controller.js';
+  updatePlant,
+} from "../controllers/plant.controller.js";
 
 const router = express.Router();
 
-router.get('/', getAllPlants);
+router.get("/", getAllPlants);
 
-router.get('/:id', getPlantById);
+router.get("/:id", getPlantById);
 
-router.get('/user/:email', getPlantsByUserEmail);
+router.get("/user/:email", getPlantsByUserEmail);
 
-router.post('/', createPlant);
+router.post("/", createPlant);
 
-router.put('/:id', updatePlant);
+router.put("/:id", updatePlant);
 
-router.delete('/:id', deletePlant);
+router.delete("/:id", deletePlant);
+
+router.get("/test", (req, res) => {
+  res.json({ message: "Still working" });
+});
 
 export default router;
